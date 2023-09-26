42874b3193b71565693334261d36fc3ec72292d1
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-09-26 14:58:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,21 @@
+let maxDiam =30;
+let minDiam = 1;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
 }
 
 function draw() {
+  background(255,200,200)
+  fill ("red")
+  noStroke()
+randomSeed(50)
+  for (let xpos = 0; xpos <= width; xpos += 0.9*maxDiam) {
+    for(let ypos= 0; ypos <=height; ypos += 0.9*maxDiam) {
+      let tDiam = random(minDiam, maxDiam);
+
+      ellipse(xpos,ypos,tDiam,tDiam)
+    }
+  }
+  
 }
